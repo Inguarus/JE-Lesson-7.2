64f0cb3383b1fe882f3b340cc68400d1632ad3e2
Igor Stognushenko <olimpiyskihigr2008@gmail.com>
2018-07-01 23:45:30 +0300

    Initial commit

---
+++ src/com/inguarus/Main.java
@@ -0,0 +1,37 @@
+package com.inguarus;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MySet<String> countries = new MyHashSet<>(15);
+
+        countries.add("USA");
+        countries.add("England");
+        countries.add("Scotland");
+        countries.add("Sweden");
+        countries.add("Ukraine");
+        countries.add("Croatia");
+        countries.add("Denmark");
+        countries.add("Brazil");
+        countries.add("Russia");
+        countries.add("Spain");
+        countries.add("Germany");
+        countries.add("Portugal");
+        countries.add("Nigeria");
+
+        System.out.println("Number of items in the list: " + countries.size() + " elements");
+        System.out.println("Countries list: ");
+        System.out.println(countries);
+        System.out.println(countries.contains("USA"));
+        System.out.println(countries.contains("USSR"));
+        countries.remove("USA");
+        countries.remove("England");
+        countries.remove("Scotland");
+        System.out.println(countries);
+        System.out.println("Countries list cleared: ");
+        countries.clear();
+        System.out.println(countries);
+
+    }
+}
---
+++ src/com/inguarus/MyHashSet.java
@@ -0,0 +1,125 @@
+package com.inguarus;
+
+public class MyHashSet<E> implements MySet<E> {
+
+    public MyHashSet(int capacity) {
+        buckets = new Entry[capacity];
+        size = 0;
+    }
+
+    private static class Entry {
+        Object key;
+        Entry next;
+    }
+
+    private Entry[] buckets;
+    private int size;
+
+
+    @Override
+    public boolean add(E e) {
+        int index = hashFunction(e.hashCode());
+        Entry current = buckets[index];
+
+        while (current != null) {
+            if (current.key.equals(e)) {
+                return false;
+            }
+            current = current.next;
+        }
+        Entry entry = new Entry();
+        entry.key = e;
+        entry.next = buckets[index];
+        buckets[index] = entry;
+        size++;
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < buckets.length; i++)
+            buckets[i] = null;
+    }
+
+    @Override
+    public boolean contains(E e) {
+        int index = hashFunction(e.hashCode());
+        Entry current = buckets[index];
+
+        while (current != null) {
+            if (current.key.equals(e)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean remove(E e) {
+        int index = hashFunction(e.hashCode());
+        Entry current = buckets[index];
+        Entry previous = null;
+
+        while (current != null) {
+            if (current.key.equals(e)) {
+                if (previous == null) {
+                    buckets[index] = current.next;
+                } else {
+                    previous.next = current.next;
+                }
+                size--;
+                return true;
+            }
+            previous = current;
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    private int hashFunction(int hashCode) {
+
+        int index = hashCode;
+        if (index < 0) {
+            index = -index;
+        }
+        return index % buckets.length;
+    }
+
+    @Override
+    public String toString() {
+        Entry currentEntry = null;
+        StringBuffer sb = new StringBuffer();
+
+        for (int index = 0; index < buckets.length; index++) {
+            if (buckets[index] != null) {
+                currentEntry = buckets[index];
+                sb.append("[" + index + "]");
+                sb.append(" " + currentEntry.key.toString());
+                while (currentEntry.next != null) {
+                    currentEntry = currentEntry.next;
+                    sb.append(" -> " + currentEntry.key.toString());
+                }
+                sb.append('\n');
+            }
+        }
+        return sb.toString();
+    }
+
+
+}
